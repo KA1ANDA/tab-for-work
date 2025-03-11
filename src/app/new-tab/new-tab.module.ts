dcc53d04f23ceb96d3e867242f02e121bc8c556d
KA1ANDA <tokokalanda20@gmail.com>
2025-03-12 01:30:44 +0400

    all pages added

--- src/app/new-tab/new-tab.module.ts
+++ src/app/new-tab/new-tab.module.ts
@@ -8,7 +8,8 @@ import { CbcReportsNewComponent } from './new-tab/cbc-reports-new/cbc-reports-ne
 import { ReportingEntityNewComponent } from './new-tab/reporting-entity-new/reporting-entity-new.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { DividerModule } from 'primeng/divider';
-
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,9 @@ import { DividerModule } from 'primeng/divider';
     TabViewModule,
     ButtonModule,
     InputTextModule,
-    DividerModule
+    DividerModule,
+    CheckboxModule,
+    InputTextareaModule
   ]
 })
 export class NewTabModule { }
